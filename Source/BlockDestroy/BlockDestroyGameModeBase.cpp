296b097daa0bc270f438973728c81efe2a7d906a
sakai0202 <51104109+sakai0202@users.noreply.github.com>
2020-05-21 09:43:49 +0900

    first commit

---
+++ Source/BlockDestroy/BlockDestroyGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "BlockDestroyGameModeBase.h"
+
---
+++ Source/BlockDestroy/BlockDestroyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlockDestroyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLOCKDESTROY_API ABlockDestroyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
